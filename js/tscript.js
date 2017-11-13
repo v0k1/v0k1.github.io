f5383372e281e6edd122985ade4e22760d405d33
v0k1 <33607810+v0k1@users.noreply.github.com>
2017-11-13 03:56:46 +0100

    Update tscript.js

--- js/tscript.js
+++ js/tscript.js
@@ -1,5 +1,3 @@
-var doc = document.getElementById("test");
-  doc.innerHTML = "Bla";
-  doc.style.backgroundColor = "yellow";
-  doc.style.color = "Red";
-  doc.style.fontSize = "30px";
+document.getElementById("test").style.backgroundColor = "yellow";
+document.getElementById("test").style.color = "Red";
+document.getElementById("test").style.fontSize = "30px";
