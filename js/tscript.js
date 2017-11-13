82a9ca961846848817bb9d846afc8c537ed646af
v0k1 <33607810+v0k1@users.noreply.github.com>
2017-11-13 03:36:17 +0100

    Update tscript.js

--- js/tscript.js
+++ js/tscript.js
@@ -1,1 +1,5 @@
-
+<noscript>Enable JavaScript for full magic!</noscript>
+var doc = document.getElementById("test");
+doc.style.backgroundcolor = "yellow";
+doc.style.color = "Red";
+doc.style.fontSize = "30px";
