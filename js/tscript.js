c5df25fe1c81932d52a06077eaa28501350e23c3
v0k1 <33607810+v0k1@users.noreply.github.com>
2017-11-13 04:41:17 +0100

    Update tscript.js

--- js/tscript.js
+++ js/tscript.js
@@ -1,3 +1,17 @@
-document.getElementById("test").style.backgroundColor = "Yellow";
-document.getElementById("test").style.color = "Red";
-document.getElementById("test").style.fontSize = "30px";
+var doc = document.getElementById("test");
+  doc.style.backgroundColor = "Yellow";
+  doc.style.color = "Red";
+  doc.style.fontSize = "30px";
+/* SideBar from W3
+function sb_open() {
+  document.getElementById("main").style.marginLeft = "180px";
+  document.getElementById("mySidebar").style.width = "180px";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function sb_close() {
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
+*/
