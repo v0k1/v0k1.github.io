3140d85fb1cd4e71399f5f9f0d50db1945d9ff1f
v0k1 <33607810+v0k1@users.noreply.github.com>
2017-11-23 20:27:06 +0100

    Update tscript.js

--- js/tscript.js
+++ js/tscript.js
@@ -2,6 +2,30 @@ var doc = document.getElementById("test");
   doc.style.backgroundColor = "Yellow";
   doc.style.color = "Red";
   doc.style.fontSize = "30px";
+
+function time(){
+    var today = new Date();
+    var h = today.getHours()
+  if (h>12) {h= h- "12"} ;
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clocky').innerHTML = h + ":" + m + ":" + s;
+  var r = parseInt(s) * 1;
+  var g = parseInt(s) * 3;
+  var b = parseInt(s) * 5;
+  document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+  var t = setTimeout(time, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+
+time();
+
 /* SideBar from W3
 function sb_open() {
   document.getElementById("main").style.marginLeft = "180px";
